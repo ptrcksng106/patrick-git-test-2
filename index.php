cd23e2a89e5838fc3378dc670c218e8fd6c19140
ptrcksng106 <sinagapatrick610@gmail.com>
2020-12-07 19:43:07 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias architecto natus magnam officia cupiditate quasi autem et vero voluptates ab eligendi, hic blanditiis sequi vel dolor delectus neque veniam?</p>
 </body>
 </html>
